bc6d040fbc04cc400f44b1f883aa9fc93a9c450f
Ana Tellez <ana.tellez.r@gmail.com>
2018-10-29 16:40:31 -0600

    tentative routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,26 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'dresses', to: 'dresses#index'
+  # get 'dresses/:id', to: 'dresses#show'
+  # get 'dresses/new', to: 'dresses#new'
+  # post 'dresses', to: 'dresses#create'
+  # get 'dresses/:id/edit', to: 'dresses#edit'
+  # patch 'dresses/:id', to 'dresses#update'
+  # delete 'dress/:id', to: 'dresses#destroy'
+  #
+  # get 'users/:id/dresses', to: 'users#dresses'
+  # post 'dresses/:id/rentals', to: 'rentals#create'
+
 end
+
+# or
+# Rails.application.routes.draw do
+# resources :users do
+#   member do
+#     get 'dresses'
+#   end
+#
+# resources :dresses do
+#   resources :rental, only: [:create]
+# end
+# end

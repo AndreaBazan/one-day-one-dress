ff082417f01f2f093961868d8f0254842735e04f
Ana Tellez <ana.tellez.r@gmail.com>
2018-10-30 12:50:28 -0600

    progress check

--- app/controllers/dresses_controller.rb
+++ app/controllers/dresses_controller.rb
@@ -1,5 +1,16 @@
 class DressesController < ApplicationController
+  before_action :set_dress, only: [:show]
   def index
     @dresses = Dress.all
   end
+
+  def show
+    @rental = Rental.new
+  end
+
+   private
+
+   def set_dress
+     @dress = Dress.find(params[:id])
+   end
 end

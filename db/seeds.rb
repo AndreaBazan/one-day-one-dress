55e1c7b4ed993fbe767eb00a65f1b0ce4099eafd
Ana Tellez <ana.tellez.r@gmail.com>
2018-10-30 11:51:17 -0600

    category added to dresses with its respective alterations to seed and user model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,16 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+categories =  %w[evening cocktail day special]
 
 10.times do
-  user = User.create(
+  user = User.create!(
     email: Faker::Internet.email,
     password: Faker::Internet.password,)
     10.times do
       dress = Dress.create(
       price: Faker::Number.between(200, 600),
       size: Faker::Number.between(1,6),
-      heigth: Faker::Number.between(150, 210),)
+      heigth: Faker::Number.between(150, 210),
+      title:Faker::Hipster.sentence(3),
+      description:Faker::Hipster.sentence(3, false, 4),
+      category: categories.sample)
       user.dresses << dress
     end
 end

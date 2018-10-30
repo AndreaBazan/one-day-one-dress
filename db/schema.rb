621ed84092bd7f509129bc5991e91d766f72cab2
Ana Tellez <ana.tellez.r@gmail.com>
2018-10-30 11:22:31 -0600

    added category to dresses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_29_220815) do
+ActiveRecord::Schema.define(version: 2018_10_30_172031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2018_10_29_220815) do
     t.integer "heigth"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
     t.index ["user_id"], name: "index_dresses_on_user_id"
   end
 

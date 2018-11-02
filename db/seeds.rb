678b969f602627e685c7173fb22272d8ebf3fcb7
Andrea Bazan <andrea.bazan.polanco@gmail.com>
2018-11-02 12:18:37 -0600

    destroy seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,23 +7,23 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 User.destroy_all
 
-10.times do
-  user = User.create!(
-    email: Faker::Internet.email,
-    password: Faker::Internet.password,)
-    5.times do
-      dress = Dress.create(
-        price: Faker::Number.between(200, 600),
-        size: Dress::SIZES.sample,
-        heigth: Faker::Number.between(150, 210),
-        title:Faker::Color.color_name,
-        description:Faker::Hipster.sentence(3, false, 4),
-        category: Dress::CATEGORIES.sample,
-        size: Dress::SIZES.sample,
-      )
-      user.dresses << dress
-    end
-end
+# 10.times do
+#   user = User.create!(
+#     email: Faker::Internet.email,
+#     password: Faker::Internet.password,)
+#     5.times do
+#       dress = Dress.create(
+#         price: Faker::Number.between(200, 600),
+#         size: Dress::SIZES.sample,
+#         heigth: Faker::Number.between(150, 210),
+#         title:Faker::Color.color_name,
+#         description:Faker::Hipster.sentence(3, false, 4),
+#         category: Dress::CATEGORIES.sample,
+#         size: Dress::SIZES.sample,
+#       )
+#       user.dresses << dress
+#     end
+# end
 
 
 

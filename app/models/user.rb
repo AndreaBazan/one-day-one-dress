b4d15e470e3e8380488592dfcd71655b78b5261f
Ana Tellez <ana.tellez.r@gmail.com>
2018-11-02 11:33:18 -0600

    search style

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  DEFAULT_PHOTO_URL = ''
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -6,4 +7,13 @@ class User < ApplicationRecord
   has_many :dresses, dependent: :destroy
   has_many :rentals
   mount_uploader :photo, PhotoUploader
+
+  def photo_or_default
+    if remote_photo_url.present?
+      photo
+    else
+      photo
+      # DEFAULT_PHOTO_URL
+    end
+  end
 end
